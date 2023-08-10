befe524e2bcbfcfb2bf488c9a174c12d8a80745f
EddieFreedome <eddie.freedome@tutanota.com>
2023-08-10 18:23:48 +0200

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ export default {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      maxWidth: {
+        '1/4':'25%',
+        '2/4':'50%',
+        '3/4':'75%',
+        '4/4':'100%',
+      },
+
+      colors: {
+        'instilla': '#08c6c0'
+      }
+    },
   },
   plugins: [],
 }
